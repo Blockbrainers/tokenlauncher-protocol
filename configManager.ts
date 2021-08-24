7057b5c04862e4c93ff109f8688f17ba975794e1
Olivier Riccini <olivierriccini@olivier.home>
2021-08-24 14:55:15 +0200

    add contract

---
+++ configManager.ts
@@ -0,0 +1,35 @@
+const fs = require('fs')
+import { TokenFactoryNetwork } from './hardhat.config'
+
+export enum TokenFactoryConfigProperty {
+  TokenFactoryAddress = 'TokenFactoryAddress',
+}
+
+const loadJSON = (network: TokenFactoryNetwork) => {
+  const filename = getFilename(network)
+  return fs.existsSync(filename) ? fs.readFileSync(filename).toString() : '{}'
+}
+
+const saveJSON = (network: TokenFactoryNetwork, json = '') => {
+  const filename = getFilename(network)
+  return fs.writeFileSync(filename, JSON.stringify(json, null, 2))
+}
+
+export const get = (network: TokenFactoryNetwork, property: TokenFactoryConfigProperty) => {
+  const obj = JSON.parse(loadJSON(network))
+  return obj[property] || 'Not found'
+}
+
+export const set = (network: TokenFactoryNetwork, property: TokenFactoryConfigProperty, value: string) => {
+  const obj = JSON.parse(loadJSON(network) || '{}')
+  obj[property] = value
+  saveJSON(network, obj)
+}
+
+export const remove = (network: TokenFactoryNetwork, property: TokenFactoryConfigProperty) => {
+  const obj = JSON.parse(loadJSON(network) || '{}')
+  delete obj[property]
+  saveJSON(network, obj)
+}
+
+const getFilename = (network: TokenFactoryNetwork) => `${__dirname}/config_${network}.json`
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,102 @@
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
-module.exports = {
-  solidity: "0.7.3",
-};
+// require('solidity-coverage')
+import 'hardhat-gas-reporter'
+import '@nomiclabs/hardhat-ethers'
+import '@nomiclabs/hardhat-waffle'
+import '@nomiclabs/hardhat-etherscan'
+import { HardhatUserConfig } from 'hardhat/types/config'
+
+require('dotenv').config()
+const mnemonic = process.env.DEV_MNEMONIC || ''
+
+export enum TokenFactoryNetwork {
+  HARDHAT = 'hardhat',
+  RINKEBY = 'rinkeby',
+  MAINNET = 'mainnet',
+  BSC_MAINNET = 'bsc-mainnet',
+  BSC_TESTNET = 'bsc-testnet',
+  KCC_MAINNET = 'kcc-mainnet',
+  KCC_TESTNET = 'kcc-testnet',
+}
+
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [
+      {
+        version: '0.8.6',
+        settings: {
+          evmVersion: 'istanbul',
+          optimizer: {
+            enabled: true,
+            runs: 1000,
+          },
+        },
+      },
+    ],
+  },
+  networks: {
+    [TokenFactoryNetwork.HARDHAT]: {
+      forking: {
+        url: 'https://bsc-dataseed.binance.org/',
+      },
+      accounts: {
+        accountsBalance: '10000000000000000000000',
+      },
+    },
+    [TokenFactoryNetwork.BSC_MAINNET]: {
+      url: 'https://bsc-dataseed.binance.org/',
+      chainId: 56,
+      accounts: {
+        mnemonic,
+      },
+      // gasPrice: 9000000000
+    },
+    [TokenFactoryNetwork.BSC_TESTNET]: {
+      // Faucet: https://testnet.binance.org/faucet-smart
+      url: 'https://data-seed-prebsc-1-s1.binance.org:8545',
+      chainId: 97,
+      accounts: {
+        mnemonic,
+      },
+    },
+    [TokenFactoryNetwork.KCC_MAINNET]: {
+      url: 'https://rpc-mainnet.kcc.network',
+      chainId: 321,
+      accounts: {
+        mnemonic,
+      },
+    },
+    [TokenFactoryNetwork.KCC_TESTNET]: {
+      // Faucet: https://faucet-testnet.kcc.network/
+      url: 'https://rpc-testnet.kcc.network',
+      chainId: 322,
+      accounts: {
+        mnemonic,
+      },
+    },
+    [TokenFactoryNetwork.RINKEBY]: {
+      url: 'https://rinkeby.infura.io/v3/' + process.env.INFURA_ID,
+      accounts: {
+        mnemonic,
+      },
+    },
+    [TokenFactoryNetwork.MAINNET]: {
+      url: 'https://mainnet.infura.io/v3/' + process.env.INFURA_ID,
+      accounts: {
+        mnemonic,
+      },
+    },
+  },
+  mocha: {
+    timeout: 99999999,
+  },
+  etherscan: {
+    apiKey: process.env.BSCCAN_APIKEY,
+  },
+  // gasReporter: {
+  //   currency: 'USD',
+  //   gasPrice: 21,
+  //   coinmarketcap: process.env.COINMARKETCAP_APIKEY,
+  // },
+}
+
+export default config
